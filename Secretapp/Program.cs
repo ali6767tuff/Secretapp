dad428628c0c9462e134ba3efda49b05af282532
ali6767tuff <hassm404@gmail.com>
2026-02-23 15:09:47 +0100

    klar

--- Secretapp/Program.cs
+++ Secretapp/Program.cs
@@ -1,103 +1,227 @@
-﻿namespace SecretApp
+﻿using System;
+using System.Threading;
+
+namespace SecretApp
 {
     internal class Program
     {
         static string[] userNames = { "Pelle", "Stina", "Ali" };
         static string[] userPasswords = { "1234", "12345", "12346" };
+        static int loggedInIndex = -1;
 
         static void Main(string[] args)
         {
-            // något kul
-            Console.WriteLine("Hej");
+            Console.WriteLine("Hej och välkommen till SecretApp!");
             Menu();
-            bool runRrogram = true;
-            while (runRrogram)
+
+            bool runProgram = true;
+
+            while (runProgram)
             {
                 if (int.TryParse(Console.ReadLine(), out int choice))
                 {
                     if (choice == 1)
-                    {
                         LoggIn();
-                    }
                     else if (choice == 2)
-                    {
                         AddUser();
-                    }
                     else if (choice == 3)
-                    {
                         DeleteUser();
-                    }
                     else if (choice == 4)
-                    {
                         ChangePassword();
-                    }
                     else if (choice == 5)
-                    {
                         ShowUsers();
-                    }
                     else if (choice == 9)
-                    {
                         Menu();
-                    }
                     else if (choice == 0)
-                    {
-                        runRrogram = false;
-                    }
+                        runProgram = false;
                 }
-
                 else
                 {
-                    Console.WriteLine("Välj i menyn");
+                    Console.WriteLine("Välj ett nummer från menyn.");
                 }
             }
+
             Console.WriteLine("Hej då.");
-            Thread.Sleep(3000);
+            Thread.Sleep(2000);
         }
 
-        //TODO Fixa att lägga till en användare
+        static void LoggIn()
+        {
+            Console.WriteLine("Inloggning");
+
+            Console.Write("Namn: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Lösenord: ");
+            string password = Console.ReadLine();
+
+            int hit = Array.IndexOf(userNames, name);
+
+            if (hit == -1)
+            {
+                Console.WriteLine("Användaren finns inte.");
+                return;
+            }
+
+            if (userPasswords[hit] == password)
+            {
+                Console.WriteLine("Välkommen " + name);
+                loggedInIndex = hit;
+            }
+            else
+            {
+                Console.WriteLine("Fel lösenord.");
+            }
+        }
+
         static void AddUser()
         {
-            Console.WriteLine("Hello from AddUser()");
+            string[] tempNames = new string[userNames.Length + 1];
+            string[] tempPasswords = new string[userPasswords.Length + 1];
+
+            Console.Write("Skriv namnet på den du vill lägga till: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Välj ett lösenord: ");
+            string password = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(password))
+            {
+                Console.WriteLine("Namn och lösenord får inte vara tomt.");
+                return;
+            }
+
+            if (Array.IndexOf(userNames, name) != -1)
+            {
+                Console.WriteLine("Användarnamnet finns redan.");
+                return;
+            }
+
+            int i = 0;
+            while (i < userNames.Length)
+            {
+                tempNames[i] = userNames[i];
+                tempPasswords[i] = userPasswords[i];
+                i++;
+            }
+
+            tempNames[tempNames.Length - 1] = name;
+            tempPasswords[tempPasswords.Length - 1] = password;
+
+            userNames = tempNames;
+            userPasswords = tempPasswords;
+
+            Console.WriteLine("Användaren är tillagd.");
         }
 
-        //TODO Fixa att ta bort en användare
         static void DeleteUser()
         {
-            Console.WriteLine("Hello from DeleteUser()");
+            if (loggedInIndex == -1)
+            {
+                Console.WriteLine("Du måste logga in först.");
+                return;
+            }
+
+            if (userNames.Length <= 1)
+            {
+                Console.WriteLine("Du kan inte radera den sista användaren.");
+                return;
+            }
+
+            Console.Write("Skriv ditt lösenord för att bekräfta borttagning: ");
+            string password = Console.ReadLine();
+
+            if (userPasswords[loggedInIndex] != password)
+            {
+                Console.WriteLine("Fel lösenord.");
+                return;
+            }
+
+            string[] tempNames = new string[userNames.Length - 1];
+            string[] tempPasswords = new string[userPasswords.Length - 1];
+
+            int i = 0;
+            int j = 0;
+
+            while (i < userNames.Length)
+            {
+                if (i == loggedInIndex)
+                {
+                    i++;
+                    continue;
+                }
+
+                tempNames[j] = userNames[i];
+                tempPasswords[j] = userPasswords[i];
+
+                i++;
+                j++;
+            }
+
+            userNames = tempNames;
+            userPasswords = tempPasswords;
+
+            loggedInIndex = -1;
+
+            Console.WriteLine("Kontot är borttaget och du är utloggad.");
+        }
+
+        static void ChangePassword()
+        {
+            if (loggedInIndex == -1)
+            {
+                Console.WriteLine("Du måste logga in först.");
+                return;
+            }
+
+            Console.Write("Skriv nuvarande lösenord: ");
+            string current = Console.ReadLine();
+
+            if (userPasswords[loggedInIndex] != current)
+            {
+                Console.WriteLine("Fel nuvarande lösenord.");
+                return;
+            }
+
+            Console.Write("Skriv nytt lösenord: ");
+            string newPassword = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(newPassword))
+            {
+                Console.WriteLine("Nytt lösenord får inte vara tomt.");
+                return;
+            }
+
+            userPasswords[loggedInIndex] = newPassword;
+
+            Console.WriteLine("Lösenordet är ändrat.");
         }
 
         static void ShowUsers()
         {
             int i = 0;
+
             while (i < userNames.Length)
             {
-                Console.WriteLine(userNames[i].ToUpper());
+                if (i == loggedInIndex)
+                    Console.WriteLine(userNames[i].ToUpper() + " (INLOGGAD)");
+                else
+                    Console.WriteLine(userNames[i].ToUpper());
+
                 i++;
             }
         }
 
-        //TODO Fixa att man ska kunna logga in
-        static void LoggIn()
-        {
-            Console.WriteLine("Hello from LogIn");
-        }
-
-        //TODO Fixa att en användare ska kunna ändra lösenord
-        static void ChangePassword()
-        {
-            Console.WriteLine("Hello from ChangePassword()");
-        }
-
         static void Menu()
         {
-            Console.WriteLine("" +
-                "1. Logga in\r\n" +
-                "2. Lägg till användare\r\n" +
-                "3. Ta bort användare\r\n" +
-                "4. Ändra lösenord\r\n" +
-                "5. Visa användarlista\r\n" +
-                "9. visa mennyn\r\n" +
-                "0. Avsluta\r\n");
-        } 
-    } 
+            Console.WriteLine(
+                "\n1. Logga in\n" +
+                "2. Lägg till användare\n" +
+                "3. Ta bort användare\n" +
+                "4. Ändra lösenord\n" +
+                "5. Visa användarlista\n" +
+                "9. Visa menyn\n" +
+                "0. Avsluta\n");
+        }
+    }
 }
